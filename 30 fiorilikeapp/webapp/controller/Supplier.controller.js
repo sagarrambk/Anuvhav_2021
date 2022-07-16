1eb05b029a28d5c0afb9f4869f1d7cc4a400e93a
sagarrambk <sagarram.bk@gmail.com>
2022-07-16 16:36:09 +0530

    Add files via upload

---
+++ 30 fiorilikeapp/webapp/controller/Supplier.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    'anubhav/app/controller/BaseController'
+], function(BaseController) {
+    'use strict';
+    return BaseController.extend("anubhav.app.controller.Supplier",{
+        onInit: function(){
+
+        }
+    });
+});
